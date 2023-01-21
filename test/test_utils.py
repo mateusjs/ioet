dd9a0ba4caef11be279b1b723c007c4ad1f29f21
mateusjs <mateusjose19@hotmail.com>
2023-01-21 15:32:20 -0300

    finished root tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -14,28 +14,44 @@ class TestUtils(unittest.TestCase):
         assert name == ""
 
     def test_should_get_days_of_work_with_success(self):
-        days = get_days_of_work('BRUNAO=MO7:00-11:00,TU14:00-16:00')
-        assert days == 'MO7:00-11:00,TU14:00-16:00'
-    
+        days = get_days_of_work("BRUNAO=MO7:00-11:00,TU14:00-16:00")
+        assert days == "MO7:00-11:00,TU14:00-16:00"
+
     def test_should_get_days_of_work_with_file_bad_formation_error(self):
         with self.assertRaises(FileWithBadFormation) as context:
-            get_days_of_work('BRUNAO')
-        self.assertTrue('File has bad formation, validate your data' in str(context.exception))
+            get_days_of_work("BRUNAO")
+        self.assertTrue(
+            "File has bad formation, validate your data" in str(context.exception)
+        )
 
     def test_should_get_hours_with_success(self):
-        start,end = get_hours('10:00-12:00')
+        start, end = get_hours("10:00-12:00")
         assert start == 10
         assert end == 12
 
     def test_should_get_hours_with_value_error(self):
         with self.assertRaises(ValueError) as context:
-            get_hours('')
-        self.assertTrue("invalid literal for int() with base 10: ''" in str(context.exception))
-    
+            get_hours("")
+        self.assertTrue(
+            "invalid literal for int() with base 10: ''" in str(context.exception)
+        )
+
     def test_should_get_hours_with_attribute_error(self):
         with self.assertRaises(AttributeError) as context:
             get_hours(None)
-        self.assertTrue("'NoneType' object has no attribute 'split'" in str(context.exception))
+        self.assertTrue(
+            "'NoneType' object has no attribute 'split'" in str(context.exception)
+        )
 
-
-        #faltou teste do shift
+    def test_should_get_the_shift_with_success(self):
+        for i in range(1, 9):
+            shift = get_the_shift(i)
+            assert shift == "morning"
+        for i in range(9, 18):
+            shift = get_the_shift(i)
+            assert shift == "evening"
+        for i in range(18, 25):
+            if i == 24:
+                shift = get_the_shift(0)
+            shift = get_the_shift(i)
+            assert shift == "night"
